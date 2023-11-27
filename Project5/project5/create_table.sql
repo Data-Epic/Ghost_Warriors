809d50c4617e6a731080642f1aa472d32ad2525f
OMOTOSHO Ayomide Quadri <122959675+LogicAL007@users.noreply.github.com>
2023-11-27 21:16:04 +0100

    Update create_table.sql
    more descriptive name for temperature

--- Project5/project5/create_table.sql
+++ Project5/project5/create_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE Weather(
     state VARCHAR(100) NOT NULL,
     country VARCHAR(100) NOT NULL,
     wind_direction VARCHAR(100) NOT NULL,
-    temp_c INT NOT NULL,
+    temperature_c INT NOT NULL,
     wind_kph FLOAT NOT NULL,
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
